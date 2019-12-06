a05d0ecda82a5498c12a21ccd4b33da74d50d286
Hugo <52917408+hugo-engineer@users.noreply.github.com>
2019-12-07 09:52:08 +1100

    update

--- server.js
+++ server.js
@@ -16,9 +16,27 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://admin:3213AAA@ds229771.mlab.com:29771/heroku_8chlrn63"
+);
 
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
+// 5!fSgWQqXh3NhXp
+// react-practice
+
+// {
+//     "_id": "heroku_8chlrn63.react-practice",
+//     "user": "react-practice",
+//     "db": "heroku_8chlrn63",
+//     "roles": [
+//         {
+//             "role": "dbOwner",
+//             "db": "heroku_8chlrn63"
+//         }
+//     ]
+// }
